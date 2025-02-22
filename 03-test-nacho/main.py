aaa2f0509bf245c24e08bf04bde12c9c573fec5a
chetex <chetex69@gmail.com>
2025-02-22 13:21:55 +0100

    New main class Test

---
+++ 03-test-nacho/main.py
@@ -0,0 +1,11 @@
+# This is the second step of python udemy course
+
+# Create a funtion with two parameters integer and calculate the sum of the two parameters
+# Explicit type of tw parameters
+def sum(a: int, b: int) -> int:
+    return a + b
+
+# Call the function with two parameters
+print(sum(1, 2))    # Print the result of the function
+
+
