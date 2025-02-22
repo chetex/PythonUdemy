616c6836787c173a266e4b2ecc022c2ec5e40574
chetex <chetex69@gmail.com>
2025-02-22 13:49:49 +0100

    New square def funtion

--- 03-test-nacho/main.py
+++ 03-test-nacho/main.py
@@ -1,11 +1,23 @@
-# This is the second step of python udemy course
+import math
 
-# Create a funtion with two parameters integer and calculate the sum of the two parameters
-# Explicit type of tw parameters
-def sum(a: int, b: int) -> int:
-    return a + b
+def calculate_square_root(num1: float, num2: float, first_number_as_main: bool = True) -> float:
+  """
+  Calcula la raíz cuadrada de un número con respecto a otro.
 
-# Call the function with two parameters
-print(sum(1, 2))    # Print the result of the function
+  Args:
+    num1: El primer número.
+    num2: El segundo número.
+    first_number_as_main: Si es True, num1 es la base y num2 el exponente. Si es False, num2 es la base y num1 el exponente.
 
+  Returns:
+    La raíz cuadrada calculada.
+  """
+  if first_number_as_main:
+    return math.pow(num1, 1 / num2)
+  else:
+    return math.pow(num2, 1 / num1)
 
+# Ejemplos de uso
+print(calculate_square_root(9, 2))  # Raíz cuadrada de 9 (resultado: 3.0)
+print(calculate_square_root(27, 3)) # Raíz cúbica de 27 (resultado: 3.0)
+print(calculate_square_root(4, 2, first_number_as_main=False)) # 2 elevado a la 1/4 (resultado: 1.189207115002721)
